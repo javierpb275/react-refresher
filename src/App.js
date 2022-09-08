7641e4584bbdb298ad8e97699fe25ba05f940fd1
javierpb275 <javierpb275@gmail.com>
2022-09-08 17:52:02 +0200

    installed react-router-dom and changed code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,5 @@
-import Todo from "./components/Todo";
-
 function App() {
-  return (
-    <div>
-      <h1>My Todos</h1>
-      <Todo text="Make bed" />
-      <Todo text="Clean house" />
-      <Todo text="Wash dishes" />
-    </div>
-  );
+  return <div></div>;
 }
 
 export default App;
