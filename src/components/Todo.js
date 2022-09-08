a206361ae22ae0549071a9ef6a6921a012b85201
javierpb275 <javierpb275@gmail.com>
2022-09-08 16:16:50 +0200

    event props (passing functions as props)

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -9,6 +9,10 @@ function Todo(props) {
     setModalIsOpen(true);
   }
 
+  function closeModalHandler() {
+    setModalIsOpen(false);
+  }
+
   return (
     <div className="card">
       <h2>{props.text}</h2>
@@ -17,8 +21,10 @@ function Todo(props) {
           Delete
         </button>
       </div>
-      {modalIsOpen && <Modal />}
-      {modalIsOpen && <Backdrop />}
+      {modalIsOpen && (
+        <Modal onCancel={closeModalHandler} onConfirm={closeModalHandler} />
+      )}
+      {modalIsOpen && <Backdrop onCancel={closeModalHandler} />}
     </div>
   );
 }
