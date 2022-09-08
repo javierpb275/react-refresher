7ae0291afb8de1223b685d54ff563d4a0237500c
javierpb275 <javierpb275@gmail.com>
2022-09-08 19:34:26 +0200

    meetupitem and meetuplist components created and used in allmeetups page

--- src/App.js
+++ src/App.js
@@ -7,17 +7,17 @@ import MainNavigation from "./components/layout/MainNavigation";
 function App() {
   return (
     <div>
-      <MainNavigation/>
+      <MainNavigation />
       <Switch>
-      <Route path="/" exact>
-        <AllMeetupsPage/>
-      </Route>
-      <Route path="/new-meetup">
-        <NewMeetupPage/>
-      </Route>
-      <Route path="/favorites">
-        <FavoritesPage/>
-      </Route>
+        <Route path="/" exact>
+          <AllMeetupsPage />
+        </Route>
+        <Route path="/new-meetup">
+          <NewMeetupPage />
+        </Route>
+        <Route path="/favorites">
+          <FavoritesPage />
+        </Route>
       </Switch>
     </div>
   );
--- src/pages/AllMeetups.js
+++ src/pages/AllMeetups.js
@@ -1,3 +1,5 @@
+import MeetupList from "../components/meetups/MeetupList";
+
 const DUMMY_DATA = [
   {
     id: "m1",
@@ -23,9 +25,7 @@ function AllMeetupsPage() {
   return (
     <section>
       <h1>All Meetups</h1>
-      {DUMMY_DATA.map((meetup) => {
-        return <li key={meetup.id}>{meetup.title}</li>;
-      })}
+      <MeetupList meetups={DUMMY_DATA} />
     </section>
   );
 }
