f91e328f65e58ab22e7d45e582ebe1f44c1cfc86
javierpb275 <javierpb275@gmail.com>
2022-09-08 15:46:27 +0200

    created backdrop and modal components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
+import Backdrop from "./components/Backdrop";
+import Modal from "./components/Modal";
 import Todo from "./components/Todo";
 
 function App() {
   return (
     <div>
       <h1>My Todos</h1>
-      <Todo text="Make bed"/>
-      <Todo text="Clean house"/>
-      <Todo text="Wash dishes"/>
+      <Todo text="Make bed" />
+      <Todo text="Clean house" />
+      <Todo text="Wash dishes" />
+      <Modal />
+      <Backdrop />
     </div>
   );
 }
