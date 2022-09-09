44457a311c49b88e4d2d55f91b86bcd88d936cfc
javierpb275 <javierpb275@gmail.com>
2022-09-09 18:38:38 +0200

    sending a post request

--- src/components/meetups/NewMeetupForm.js
+++ src/components/meetups/NewMeetupForm.js
@@ -2,7 +2,7 @@ import Card from "../ui/Card";
 import classes from "./NewMeetupForm.module.css";
 import { useRef } from "react";
 
-function NewMeetupForm() {
+function NewMeetupForm(props) {
   const titleInputRef = useRef();
   const imageInputRef = useRef();
   const addressInputRef = useRef();
@@ -21,7 +21,7 @@ function NewMeetupForm() {
       description: enteredDescription,
     };
 
-    console.log(meetupData);
+    props.onAddMeetup(meetupData);
   }
   return (
     <Card>
--- src/pages/NewMeetup.js
+++ src/pages/NewMeetup.js
@@ -1,10 +1,20 @@
 import NewMeetupForm from "../components/meetups/NewMeetupForm";
+import { API_BASE_URL } from "../config/constants";
 
 function NewMeetupPage() {
+  function addMeetupHandler(meetupData) {
+    fetch(API_BASE_URL + "meetups.json", {
+      method: "POST",
+      body: JSON.stringify(meetupData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  }
   return (
     <section>
       <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+      <NewMeetupForm onAddMeetup={addMeetupHandler} />
     </section>
   );
 }
